2ee0e8e1b867496ff44fccb19021b8fadf6bc471
Zand <zabahey.sukoi@gmail.com>
2018-01-17 23:26:47 +0700

    separate component structure for create video page

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Create a new component. This component should produce some HTML
-const App = function() {
-    return <div>Hi!</div>;
-}
+const App = () => <div>Hi!</div>
 
 // Take this component's generated HTML and put it on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
