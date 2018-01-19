04e012e1f1d5710446ee478cc4cd380d2a0ae771
zand <zabahey.sukoi@gmail.com>
2018-01-19 22:47:45 +0700

    convert functional component to class base component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
-const SearchBar= () => {
-    return <input />
-};
+class SearchBar extends React.Component {
+    render() {
+        return <input />;
+    }
+}
 
 export default SearchBar;
 
