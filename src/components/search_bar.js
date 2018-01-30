a0253e3c5f7ff6821ff717dd3e33ab18250a066f
zand <zabahey.sukoi@gmail.com>
2018-01-30 23:03:39 +0700

    set value of textbox to state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -8,7 +8,7 @@ class SearchBar extends Component {
     }
 
     render() {
-        return <input onChange={event => console.log(event.target.value)} />;
+        return <input onChange={event => this.setState({term: event.target.value})} />;
     }
 }
 
