fd7046a061cb1ec7d073ca7557cc6112f4cb9452
zand <zabahey.sukoi@gmail.com>
2018-02-06 22:00:12 +0700

    refactor from functional component to class base component

--- src/index.js
+++ src/index.js
@@ -1,24 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar';
-
 const API_KEY = '';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-    console.log(data);
-});
+class App extends Component {
+    constructor(props) {
+        super(props);
 
-// Create a new component. This component should produce some HTML
-const App = () => {
-return (
-    <div>
-        <SearchBar />
-    </div>
-    );
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, videos => {
+            this.setState({ videos });
+            //this.setState({ videos: videos }); 
+            //only work when the key and the property are the same variable name
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
 }
 
-
-// Take this component's generated HTML and put it on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
