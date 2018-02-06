b18743e7bfa8a529b0d888b25bdce43e46eb44ce
zand <zabahey.sukoi@gmail.com>
2018-02-06 22:28:13 +0700

    display video list item and set key to identify each elements

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
+    var videoItems = props.videos.map((video) => {
+        return <VideoListItem key={video.etag} video={video} />;
+    });
     return (
         <ul className="col-md-4 list-group">
-        {props.videos.length}
+            {videoItems}
         </ul> 
     );
 };
